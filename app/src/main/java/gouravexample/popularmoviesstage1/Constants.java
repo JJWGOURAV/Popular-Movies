847438a0e67821925c18c00221d4198176aa3518
Gourav Jhunjhunwala <gourav@navya.co>
2016-09-02 09:03:53 +0530

    Adding Constants.java file to Github to fix compiling issues.

---
+++ app/src/main/java/gouravexample/popularmoviesstage1/Constants.java
@@ -0,0 +1,10 @@
+package gouravexample.popularmoviesstage1;
+
+/**
+ * Created by GOURAV on 19-08-2016.
+ */
+public class Constants {
+
+    public static final String THEMOVIEDB_API_KEY = "XXXXXXX";
+    public static final String SESSION_ID = "XXXXXX";
+}
